f16a496c3b3c331334cfa8a335a667d74689ece5
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-09-20 01:01:49 +0530

    state, store and loading rendering done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+/* eslint-disable no-unused-vars */
+import React, {useEffect, useState} from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components"
+ 
+
+
 function App() {
-  
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+      authService.getCurrentUser().then(
+        (userData) => {
+          if (userData) {
+            dispatch(login({userData}))
+          }else{
+            dispatch(logout())
+          }
+        }
+      ).finally(() => setLoading(false))
+  },[])
 
-  return (
-    <>
-      <h1>A mega blog project</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* todo */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
